4624dffac6437b9d006be92e2bbd5789ce341a41
Maxime Schellenberger <maxschell31@gmail.com>
2020-05-23 17:35:56 +0200

    update section 3 bgcolor-card

--- chordeus/src/components/ctn-card-info.js
+++ chordeus/src/components/ctn-card-info.js
@@ -4,12 +4,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 function CardInfo(props){
+    const ctnCard = {
+        backgroundColor: props.bgColor
+    }
+    const buttonColor = {
+        color: props.color
+    }
     return (
-        <div className="ctn-card-info">
+        <div className="ctn-card-info" style={ctnCard}>
             <div className="ctn-card-inner">
                 <span className="title-card-info">{props.title}</span>
                 <p className="para-card-info">{props.info}</p>
-                <button className="more">En savoir plus<FontAwesomeIcon icon={faArrowRight} className="icon"/></button>
+                <button className="more" style={buttonColor}>En savoir plus<FontAwesomeIcon icon={faArrowRight} className="icon"/></button>
             </div>
         </div>
     )
