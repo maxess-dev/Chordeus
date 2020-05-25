a77cb846e0bcb3420ab6c6b76bae1b523460a268
Maxime Schellenberger <maxschell31@gmail.com>
2020-05-25 18:58:40 +0200

    Layout 3 80% finished

--- chordeus/src/components/ctn-card-info.js
+++ chordeus/src/components/ctn-card-info.js
@@ -2,11 +2,9 @@ import React from 'react';
 import '../css/card-info.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
-import guitar from '../img/guitar.png';
-import drums from '../img/drums.png';
-import synth from '../img/synth.png';
 
 function CardInfo(props){
+
     const ctnCard = {
         backgroundColor: props.bgColor,
         marginLeft: props.marginLeft
@@ -17,11 +15,17 @@ function CardInfo(props){
         color: props.color
     }
 
+    const instrumentPosition = {
+        bottom: props.bottomInstrument,
+        width: props.widthInstrument,
+        right: props.rightInstrument
+    }
+
     return (
         <div className="ctn-card-info" style={ctnCard}>
-            <img src={guitar} alt="Guitar" className="guitar"/>
+            <img src={props.instrument} alt="Guitar" className="guitar" style={instrumentPosition}/>
             <div className="ctn-card-inner">
-                <span className="title-card-info">{props.title}</span>
+                <span className="title-card-info">{props.title}<FontAwesomeIcon icon={faArrowRight} className="icon-title"/></span>
                 <p className="para-card-info">{props.info}</p>
                 <button className="more" style={buttonMargin}>En savoir plus<FontAwesomeIcon icon={faArrowRight} className="icon"/></button>
             </div>
