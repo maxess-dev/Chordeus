3c325d4d728c7d57bffcc824f6019fc283cd54cd
Maxime Schellenberger <maxschell31@gmail.com>
2020-05-25 19:28:41 +0200

    landing page completed

--- chordeus/src/components/homepage.js
+++ chordeus/src/components/homepage.js
@@ -52,7 +52,14 @@ function Home() {
                     <CardInfo icon={faHandshake} widthInstrument="300px" instrument={guitar} title="Fiable" info="Notre politique de mise en vente impose aux vendeurs de publier une vidéo démontrant la qualité de leurs intruments." bgColor="#3B6EF8" color="#4267CC" marginTop="35px" marginLeft ="50px"/>
                     <CardInfo icon={faTree} widthInstrument="200px" instrument={drums} title="Eco-Friendly" info="L’achat d’un produit d'occasion permet de réduire les pollutions liées à la création de nouveaux instruments." bgColor="#5CD856" color ="#56BF59" marginTop ="35px" marginLeft ="50px" bottomInstrument ="-100px"/>
                     <CardInfo icon={faSmile} widthInstrument="350px" instrument={synth} title="Abordable" info="En tant qu’étudiant, nous nous engageons à rendre abordable au plus grand nombre la musique, grâce à des instruments qui s’accordent à votre budget." bgColor="#E66060" color="#E97B7B" marginLeft ="50px" bottomInstrument ="230px" rightInstrument = "20px"/>
-                </div>        
+                </div>      
+                <footer>
+                    <h3 className ="footer-title-light">Chercher <span className="footer-title-bold"> votre </span>instrument dès <span className="footer-title-bold">maintenant !</span></h3>
+                    <div className ="cta-register no-margin">
+                        <button>Acheter</button>
+                        <button>Vender</button>
+                    </div>
+                </footer>
             </section>
         </main>
         
